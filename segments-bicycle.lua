b1207d352bf9fdf9a9f9269ca09d58313b9a1a58
Michal Palenik <michal.palenik@freemap.sk>
2017-04-07 23:28:52 +0200

    nicer elevation in bike profile

--- segments-bicycle.lua
+++ segments-bicycle.lua
@@ -14,11 +14,14 @@ function segment_function (segment)
 	--print(segment.distance .. " " .. segment.weight)
 	if row and row.prevysenie then
 		local slope = tonumber(row.prevysenie)/segment.distance
-		if slope < 0.005 then
-			segment.weight = segment.weight ;
-	        else
-			segment.weight=segment.weight*math.max(0, (1+20*slope));
-        	end
+		--if slope < 0.005 then segment.weight = segment.weight; else segment.weight=segment.weight*math.max(0, (1+20*slope)); end
+		local ele_gain = tonumber(row.prevysenie);
+		local extra = (ele_gain*0.028 + 0.00036*ele_gain*ele_gain/(segment.distance/1000) )*60 ; -- extra in seconds due to elevation gain
+        if slope > 0.01 then
+            segment.weight=segment.weight + 0.7*extra--*segment.weight/segment.duration;
+        end
+        -- time in seconds
+		segment.duration = segment.duration + extra;
 		--print(slope .. " " .. segment.weight .. " " ..segment.duration)
 	end
 	cursor:close();
