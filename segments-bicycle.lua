2fc243f8f1fe363310af9d246834d1d544e7e4b1
Michal Palenik <michal.palenik@freemap.sk>
2017-04-14 07:52:28 +0200

    more accurate bicycle speeds for elevation

--- segments-bicycle.lua
+++ segments-bicycle.lua
@@ -7,28 +7,27 @@ print("PostGIS connection opened")
 
 function segment_function (segment)
 	local sql_query = "select (getz(ST_SetSRID(st_makepoint(" .. segment.target.lon .. "," .. segment.target.lat .. "), 4326)::geography) " .. 
-		" - getz(ST_SetSRID(st_makepoint(" .. segment.source.lon .. "," .. segment.source.lat .. "), 4326)::geography)) as prevysenie" 
+		" - getz(ST_SetSRID(st_makepoint(" .. segment.source.lon .. "," .. segment.source.lat .. "), 4326)::geography)) as ele_gain" 
 	--	.. ", ";
 	local cursor = assert( sql_con:execute(sql_query) )   -- execute querty
 	local row = cursor:fetch( {}, "a" )                   -- fetch first (and only) row
 	--print(segment.distance .. " " .. segment.weight)
-	if row and row.prevysenie then
-		local slope = tonumber(row.prevysenie)/segment.distance
-		--if slope < 0.005 then segment.weight = segment.weight; else segment.weight=segment.weight*math.max(0, (1+20*slope)); end
-		local ele_gain = tonumber(row.prevysenie);
-		local extra = (ele_gain*0.028 + 0.00036*ele_gain*ele_gain/(segment.distance/1000) )*60 ; -- extra in seconds due to elevation gain
-        if slope > 0.01 then
+	if row and row.ele_gain then
+		local slope = 100*tonumber(row.ele_gain)/segment.distance
+		--if slope > 4 then print(slope .. " " .. segment.weight .. " " ..segment.duration .. " speed:" .. segment.distance/segment.duration) end
+		if slope > 9 then slope=9 end
+		if slope < -9 then slope=-9 end
+		local extra = 3.6*segment.distance*(-1/17.1 + 1/(17.1 -3.797210*slope +0.212318*slope*slope +0.015032*slope*slope*slope -0.001251*slope*slope*slope*slope)); 
+		-- extra is in seconds due to elevation gain
+        if slope > 0.1 then
             segment.weight=segment.weight + 0.7*extra--*segment.weight/segment.duration;
         end
         -- time in seconds
 		segment.duration = segment.duration + extra;
-		--print(slope .. " " .. segment.weight .. " " ..segment.duration)
+		--if math.abs(slope) > 4 then print(slope .. " " .. extra .. " " .. segment.weight .. " " ..segment.duration .. "dist: " .. segment.distance .." speed:" .. segment.distance/segment.duration) end
+		--if slope < -2 then segment.duration = segment.duration /3 end
 	end
 	cursor:close();
 end
 		
--- pre turistov: t = 14 . d  + 0,028 . h + 0,00036 . h2 / d
--- kde: t - je čas chôdze v minútach, d - je dĺžka úseku s rovnakým sklonom v km, h - je rozdiel nadmorských výšok konca a začiatku úseku v m (+ alebo - )
--- alebo http://www.pohora.cz/16139-jak-odhadnout-dobu-trvani-tury/
--- bicykel: http://www.kreuzotter.de/english/espeed.htm
-
+-- bike speed formula is from http://www.kreuzotter.de/english/espeed.htm
