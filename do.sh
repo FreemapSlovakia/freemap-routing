6defe063953914ea1d49b5834c4819f1d2c4d723
Michal Palenik <michal.palenik@freemap.sk>
2017-06-30 12:26:13 +0200

    remove stale files after build

--- do.sh
+++ do.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # prefix used by osm2pgsql --slim
+
+if [ `ps ax| grep freemap-routing/do.sh | grep -v grep | wc -l` -gt 2 ]; then
+ echo "script already running `ps ax| grep freemap-routing/do.sh|grep -v grep `";
+ exit;
+fi
+
+
 dbname='mapnik';
 prefix='osrm_osm';
 datadir='/home/ssd/osrm'
@@ -17,7 +24,6 @@ cd $SCRIPTPATH
 #echo "select 'bicycle_ways={' || string_agg(distinct concat('[', parts::text, ']=\"', name, '\"' ), ', ') || '};' from ( select first(name order by dlzka desc) as name, parts from (select name,unnest(parts) as parts, dlzka from (select osm_id, replace(name, '\"', '') as name, round(st_length(way)) as dlzka from trasy where typ='cyklotrasa' ) as f,${prefix}rels where osm_id*-1 = id and osm_id < 0) as t group by parts) as tt ;" | psql -t mapnik > tmp/route_bicycle.lua
 
 #echo "select 'crossings={' || string_agg(distinct concat('[', id::text, '] = TRUE'), ', ') || '};' from (select nodes.id, (unnest(ways.tags)) as tags from fresh_osm_nodes as nodes, fresh_osm_ways as ways where nodes.tags && array['crossing'] and ways.nodes && array[nodes.id]) as t where array[tags] && array['primary','secondary','tertiary'] ;"| psql -t mapnik > tmp/crossing.lua
-
 
 d=`date --date="today" +"%g%m%d"`
 scp -p -P 21122 92.240.244.41:/freemap/datastore.fm/httpd/dev/tmp/osmosis/planet/bigslovakia$d.pbf $datadir/ttt.pbf
@@ -49,6 +55,7 @@ out="$out,bicycle profile $f:\t`date`"
 profile=bicycle; mkdir -p $datadir/tmp-$profile; cp $datadir/$f.pbf $datadir/tmp-$profile/
 STXXLCFG="stxxl-$profile"; echo "disk=/tmp/stxxl-$profile,2G,memory" > $STXXLCFG
 osrm-extract -p oma-$profile.lua $datadir/tmp-$profile/$f.pbf && osrm-contract $datadir/tmp-$profile/$f.osrm && mv $datadir/tmp-$profile/$f.osrm* $datadir/$profile/ && killall osrm-routed
+rm $datadir/tmp-$profile/*pbf
 
 echo "FOOT routing"
 #f="bratislava";
@@ -56,6 +63,8 @@ out="$out,foot profile $f: `date`"
 profile=foot; mkdir -p $datadir/tmp-$profile; cp $datadir/$f.pbf $datadir/tmp-$profile/
 STXXLCFG="stxxl-$profile"; echo "disk=/tmp/stxxl-$profile,2G,memory" > $STXXLCFG
 osrm-extract -p oma-$profile.lua $datadir/tmp-$profile/$f.pbf && osrm-contract $datadir/tmp-$profile/$f.osrm && mv $datadir/tmp-$profile/$f.osrm* $datadir/$profile/ && killall osrm-routed
+rm $datadir/tmp-$profile/*pbf
+
 
 echo "test routing"
 out="$out,test profile $f: `date`"
