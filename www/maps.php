5235762983083b39631e77670af0355e3170fd8b
Michal Palenik <michal.palenik@freemap.sk>
2017-11-06 10:53:02 +0100

    ski routing first implementation: server status

--- www/maps.php
+++ www/maps.php
@@ -5,6 +5,7 @@ $speed['bike'] = array('speeds' => array(5,10), 'colors' => array('blue','green'
 $speed['car'] = array('speeds' => array(30,50), 'colors' => array('red','blue', 'green') );
 $speed['foot'] = array('speeds' => array(3,7), 'colors' => array('blue','green','red') );
 $speed['test'] = array('speeds' => array(3,7), 'colors' => array('blue','green','red') );
+$speed['ski'] = array('speeds' => array(10,20), 'colors' => array('blue','green','red') );
 
 
 //rate
