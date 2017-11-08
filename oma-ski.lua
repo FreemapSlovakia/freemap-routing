bfe21b46b45c4bf77e13eacaa6fbddda95c6d757
Michal Palenik <michal.palenik@freemap.sk>
2017-11-08 09:16:44 +0100

    functional nordic skiing?

--- oma-ski.lua
+++ oma-ski.lua
@@ -21,7 +21,7 @@ function WayHandlers.skiaerialway(profile,way,result,data)
 	if data.aerialway == 'gondola' or data.aerialway == 'cable_car' or data.aerialway == 'mixed_lift' then
 		result.forward_classes['gondola'] = true; result.backward_classes['gondola'] = true;
 		result.backward_mode = mode.ferry;
-		result.backward_speed=result.forward_speed; result.backward_rate=result.backward_speed/10;
+		result.backward_speed=result.forward_speed; result.backward_rate=result.forward_rate/10;
 	elseif data.aerialway == 'chair_lift' then
 		result.forward_classes['chairlift'] = true;
 	elseif data.aerialway == 't-bar' or data.aerialway == 'j-bar' or data.aerialway == 'platter' or data.aerialway == 'drag_lift' then
@@ -54,35 +54,39 @@ function WayHandlers.skipiste(profile,way,result,data)
                 result.backward_speed=3; result.backward_rate=3;
                 result.backward_mode = mode.driving; result.forward_mode = mode.driving;
 		result.name = result.name .. ' ⋯';
-		return;
+		return result;
 	end
 	if data.piste == 'downhill' then
 		result.forward_speed=30; result.forward_rate=30;
 		result.forward_mode = mode.driving;
 		result.name = result.name .. ' ⟿';
-		result.backward_mode = mode.inaccessible;
+		result.backward_speed = 1/10; result.backward_rate = 1/100;
+		result.backward_mode = mode.driving;
 		-- todo: class dificulty
-		return;
+		return result;
 	end
 end
 
 function WayHandlers.skinordic(profile,way,result,data)
-	if not data.piste == 'nordic' then
+	if not data.piste or not data.piste == 'nordic' then
 		return
 	end
+	if data.piste == 'nordic' then
 	result.forward_speed=5; result.forward_rate=5;
 	result.backward_speed=5; result.backward_rate=5;
 	result.backward_mode = mode.driving; result.forward_mode = mode.driving;
 	result.forward_classes['nordic'] = true; result.backward_classes['nordic'] = true;
+	end
 end
 
 function setup()
   return {
     properties = {
 		weight_name = 'routability',
+		--force_split_edges = true
     }, 
     default_mode = mode.ferry,
-    default_speed = 30,
+    default_speed = 1,
     classes = Sequence {
         'gondola', 'chairlift', 'platter', 'child', 'nordic'
     },
@@ -109,17 +113,30 @@ function process_way(profile, way, result, relations)
 	if way:get_value_by_key('railway') == 'funicular' then
 		data.aerialway = 'gondola'
 	end
-    if ( not data.aerialway or data.aerialway =='') and ( not data.piste or data.piste == '') then
-	return
-    end
-    handlers = Sequence {
+	-- data.piste from relation
+	if not data.piste or data.piste == '' then
+		local rel_id_list = relations:get_relations(way)
+		for i, rel_id in ipairs(rel_id_list) do
+			local rel = relations:relation(rel_id);
+			local p = rel:get_value_by_key("piste:type");
+			if p then
+				data.piste = p;
+			end
+		end
+	end
+
+	if ( not data.aerialway or data.aerialway =='') and ( not data.piste or data.piste == '') then
+		return
+	end
+	handlers = Sequence {
 		WayHandlers.default_mode,
 		WayHandlers.names,
 		--WayHandlers.oneway,
 		WayHandlers.skiaerialway,
-		WayHandlers.skipiste
-    }
-    WayHandlers.run(profile, way, result, data, handlers, relations)
+		WayHandlers.skipiste,
+		WayHandlers.skinordic
+	}
+	WayHandlers.run(profile, way, result, data, handlers, relations)
 end
 
 return {
