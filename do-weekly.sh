4b3a7ef671be3b29d53550a44d6b40c8389998b5
Michal Palenik <michal.palenik@freemap.sk>
2017-12-05 11:27:52 +0100

    cd bug solved

--- do-weekly.sh
+++ do-weekly.sh
@@ -10,6 +10,7 @@ out=" starting:\t`date`"
 #download data - done by daily script
 postgis_import; upgrade_osrm bicycle
 # probably there are new data available
+cd $SCRIPTPATH
 postgis_import; upgrade_osrm foot
 
 out="$out,end:\t`date`"
