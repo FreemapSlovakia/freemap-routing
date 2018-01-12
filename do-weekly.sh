86d2771dcb3d328a6f983520ab9fe8b1f16071b4
Michal Palenik <michal.palenik@freemap.sk>
2018-01-12 18:05:28 +0100

    use bikesharing.pbf for foot and bike routing

--- do-weekly.sh
+++ do-weekly.sh
@@ -8,10 +8,12 @@ date
 out=" starting:\t`date`"
 
 #download data - done by daily script
-postgis_import; upgrade_osrm bicycle
+# cca 7.5 hours
+postgis_import; cp $datadir/bikesharing.pbf $datadir/tmp-bicycle/bigslovakia.pbf; upgrade_osrm bicycle
 # probably there are new data available
 cd $SCRIPTPATH
-postgis_import; upgrade_osrm foot
+# cca 22 hours
+postgis_import; cp $datadir/bikesharing.pbf $datadir/tmp-foot/bigslovakia.pbf; upgrade_osrm foot
 
 out="$out,end:\t`date`"
 echo $out | sed 's/,/\n/g'
