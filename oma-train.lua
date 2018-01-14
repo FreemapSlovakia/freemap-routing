639a2583bb85599c8ee33b1afb0daaf9ca46f042
Michal Palenik <michal.palenik@freemap.sk>
2018-01-14 15:48:03 +0100

    change train profile to api v 4

--- oma-train.lua
+++ oma-train.lua
@@ -1,19 +1,19 @@
 -- OSRM tran and tram profile
 
-api_version = 3
+api_version = 4
 Set = require('lib/set')
 Sequence = require('lib/sequence')
 Handlers = require("lib/way_handlers")
 --Relations = require("lib/relations")
 
-function WayHandlers.train(profile,way,result,data)
+function WayHandlers.train(profile,way,result,data, relations)
 	if not data.railway or data.railway =='' or data.railway == 'platform' then
 		return;
 	end
 	local service = way:get_value_by_key('service');
 	if service == 'siding' or service == 'yard' or service == 'spur' then return false; end
 	result.forward_speed=profile.default_speed; result.forward_rate=profile.default_speed;
-    result.backward_speed=profile.default_speed; result.backward_rate=profile.default_speed;
+	result.backward_speed=profile.default_speed; result.backward_rate=profile.default_speed;
 	if way:get_value_by_key('usage') == 'main' then
 		result.forward_rate=result.forward_rate*2;
 		result.backward_rate=result.backward_rate*2;
