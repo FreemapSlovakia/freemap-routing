a9b4d3cf3c2ef973056284a880847b4af6b51595
Michal Palenik <michal.palenik@freemap.sk>
2018-01-14 15:50:01 +0100

    make foot profile test

--- do-test.sh
+++ do-test.sh
@@ -8,8 +8,12 @@ out=" starting: `date`"
 
 out="$out,get pistes: `date`"
 
-small=10
-upgrade_osrm ski
+cat master-ski.lua | grep -v 'grep nordic' > $osrmdir/oma-ski.lua
+#small=10; upgrade_osrm ski
+cp *lua $osrmdir/
+cp $osrmdir/oma-foot.lua $osrmdir/oma-test.lua
+cp $datadir/bratislava.pbf $datadir/tmp-test/bigslovakia.pbf && upgrade_osrm test
+#cp $datadir/bigslovakia.pbf $datadir/tmp-bus/bigslovakia.pbf && upgrade_osrm bus
 
 
 out="$out,end: `date`"
