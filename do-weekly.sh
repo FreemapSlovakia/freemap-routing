2292d17631a027e5e633dfc628626cb156baa0aa
Michal Palenik <michal.palenik@freemap.sk>
2018-02-15 21:11:31 +0100

    change weekly order

--- do-weekly.sh
+++ do-weekly.sh
@@ -8,12 +8,12 @@ date
 out=" starting:\t`date`"
 
 #download data - done by daily script
-# cca 7.5 hours
-postgis_import; cp $datadir/bikesharing.pbf $datadir/tmp-bicycle/bigslovakia.pbf; upgrade_osrm bicycle
-# probably there are new data available
-cd $SCRIPTPATH
 # cca 22 hours
 postgis_import; cp $datadir/bikesharing.pbf $datadir/tmp-foot/bigslovakia.pbf; upgrade_osrm foot
+# probably there are new data available
+cd $SCRIPTPATH
+# cca 7.5 hours
+postgis_import; cp $datadir/bikesharing.pbf $datadir/tmp-bicycle/bigslovakia.pbf; upgrade_osrm bicycle
 
 out="$out,end:\t`date`"
 echo $out | sed 's/,/\n/g'
