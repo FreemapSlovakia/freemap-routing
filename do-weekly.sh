d974d26f56d3d6ba949fe62df2593ffc6418df83
Michal Palenik <michal.palenik@freemap.sk>
2018-11-05 17:28:54 +0100

    more procise hours for profiles

--- do-weekly.sh
+++ do-weekly.sh
@@ -8,12 +8,12 @@ date
 out=" starting:\t`date`"
 
 #download data - done by daily script
-# cca 22 hours
+# cca 28 hours
 postgis_import; cp $datadir/bikesharing.pbf $planetdir/tmp-foot/bigslovakia.pbf; upgrade_osrm foot
 # probably there are new data available
 cd $SCRIPTPATH
-# cca 7.5 hours
-postgis_import; cp $datadir/bikesharing.pbf $planetdir/tmp-bicycle/bigslovakia.pbf; upgrade_osrm bicycle
+# cca 9 hours
+date; postgis_import; cp $datadir/bikesharing.pbf $planetdir/tmp-bicycle/bigslovakia.pbf; upgrade_osrm bicycle
 
 out="$out,end:\t`date`"
 echo $out | sed 's/,/\n/g'
