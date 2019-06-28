97b1d3be62faf3a76378885140682d18f4342e84
Michal Palenik <michal.palenik@freemap.sk>
2019-06-28 16:50:02 +0200

    only foot profile in weekly run

--- do-weekly.sh
+++ do-weekly.sh
@@ -9,11 +9,12 @@ out=" starting:\t`date`"
 
 #download data - done by daily script
 # cca 28 hours
-postgis_import; cp $datadir/bikesharing.pbf $planetdir/tmp-foot/bigslovakia.pbf; upgrade_osrm foot
+postgis_import;
+cp $datadir/bikesharing.pbf $planetdir/tmp-foot/bigslovakia.pbf; upgrade_osrm foot
 # probably there are new data available
 cd $SCRIPTPATH
 # cca 9 hours
-date; postgis_import; cp $datadir/bikesharing.pbf $planetdir/tmp-bicycle/bigslovakia.pbf; upgrade_osrm bicycle
+#date; postgis_import; cp $datadir/bikesharing.pbf $planetdir/tmp-bicycle/bigslovakia.pbf; upgrade_osrm bicycle
 
 out="$out,end:\t`date`"
 echo $out | sed 's/,/\n/g'
