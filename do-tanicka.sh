48fe958028c62672c466c232ba269240feeb677c
Michal Palenik <michal.palenik@freemap.sk>
2019-09-28 21:36:33 +0200

    run bus/train profile on BA region only

--- do-tanicka.sh
+++ do-tanicka.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 SCRIPT=$(readlink -f "$0"); SCRIPTPATH=$(dirname "$SCRIPT")
+cd $SCRIPTPATH
+. $SCRIPTPATH/library.sh
 
 date
 out=" starting: `date`"
 
 cd $datadir
-rm bigslovakia.pbf
-wget -q https://routing.freemap.sk/data/bigslovakia.pbf
+rm bratislava.pbf
+wget -q https://routing.freemap.sk/data/bratislava.pbf
+mv slovakia.pbf slovakia.pbf-old; wget -O slovakia.pbf -q https://routing.freemap.sk/data/oma.pbf
 
 cd $SCRIPTPATH
-. $SCRIPTPATH/library.sh
 
-crop_slovakia > /dev/null
+#crop_slovakia > /dev/null
 
-test_file > /dev/null
-rm $planetdir/tmp-bus/* $planetdir/tmp-train/*
-cat $osrmdir/osrm-backend/profiles/car.lua |grep -v area > $osrmdir/oma-car.lua
-upgrade_osrm bus > /dev/null
+# if bigslovakia is big enough
+
+rm $planetdir/tmp-*/*
+
+cp $datadir/bratislava.pbf $planetdir/tmp-bus/bigslovakia.pbf
+upgrade_osrm bus
 
 small=100
+cp $datadir/bratislava.pbf $planetdir/tmp-train/bigslovakia.pbf
+
 upgrade_osrm train > /dev/null
 
 out="$out,end: `date`"
